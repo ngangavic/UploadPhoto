fb98c95d9b7df90ef03beb9698db025a51e2c211
Ng'ang'a Victor <ngangavictor10@gmail.com>
2019-08-08 22:59:31 +0300

    Server files

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php   
+//Define your host here.
+$servername = "localhost";
+//Define your database username here.
+$username = "root";
+//Define your database password here.
+$password = "";
+//Define your database name here.
+$dbname = "android";
+
+$conn = mysqli_connect($servername,$username,"",$dbname);
+ 
+?>
